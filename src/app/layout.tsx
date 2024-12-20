344e341ec4ff712e980598ce45bcafb87749a2ca
cashlessman <cashlessman@proton.me>
2024-12-20 21:25:23 +0530

    5th commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "~/app/globals.css";
 import { Providers } from "~/app/providers";
 
 export const metadata: Metadata = {
-  title: "Farcaster Frames v2 Demo",
-  description: "A Farcaster Frames v2 demo app",
+  title: "layout.tsx title",
+  description: "layout.tsx description",
 };
 
 export default async function RootLayout({
